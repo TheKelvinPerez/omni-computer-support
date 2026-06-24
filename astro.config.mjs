6300e85e390c891e7933b050fe5ef8344e807679
TheKelvinPerez <thekelvinperez@gmail.com>
2026-06-24 11:49:01 -0500

    Remove GitHub Pages deployment workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,5 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: "https://thekelvinperez.github.io",
-  base: "/omni-computer-support",
   output: "static",
 });
